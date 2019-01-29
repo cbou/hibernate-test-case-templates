3ab1ddc15ee3146b350293b475ca30aef33a974e
Charles Bourasseau <charles.bourasseau@gmail.com>
2019-01-29 10:10:31 +0100

    add failing and working test

--- orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import org.hibernate.bugs.domain.Folder;
 import org.hibernate.bugs.domain.Project;
 import org.hibernate.bugs.domain.User;
 import org.junit.After;
@@ -30,7 +31,7 @@ public class JPAUnitTestCase {
 	// Entities are auto-discovered, so just add them anywhere on class-path
 	// Add your tests, using standard JUnit.
 	@Test
-	public void hhh123Test() throws Exception {
+	public void failingTest() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
 		// Do stuff...
@@ -38,23 +39,73 @@ public class JPAUnitTestCase {
 		user.setName("My user");
 		entityManager.persist(user);
 
+		Folder folder = new Folder();
+		folder.setName("My folder");
+		entityManager.persist(folder);
+
 		Project project = new Project();
 		project.setName("My project");
 		project.setOwner(user);
+		project.setFolder(folder);
 		entityManager.persist(project);
 
 		project = new Project();
 		project.setName("My project");
 		project.setOwner(user);
+		project.setFolder(folder);
 		entityManager.persist(project);
 
 		assertEquals("My user", project.getOwner().getName());
 		assertEquals(2, user.getProjects().size());
+		assertEquals(2, folder.getProjects().size());
 
 		entityManager.remove(project);
 		entityManager.flush();
 
+		assertEquals(1, folder.getProjects().size());
+		assertEquals(1, user.getProjects().size());
+
+		entityManager.getTransaction().commit();
+		entityManager.close();
+	}
+
+	@Test
+	public void workingTest() throws Exception {
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		// Do stuff...
+		User user = new User();
+		user.setName("My user");
+		entityManager.persist(user);
+
+		Folder folder = new Folder();
+		folder.setName("My folder");
+		entityManager.persist(folder);
+
+		Project project = new Project();
+		project.setName("My project");
+		project.setOwner(user);
+		project.setFolder(folder);
+		entityManager.persist(project);
+
+		project = new Project();
+		project.setName("My project");
+		project.setOwner(user);
+		project.setFolder(folder);
+		entityManager.persist(project);
+
 		assertEquals("My user", project.getOwner().getName());
+		assertEquals(2, user.getProjects().size());
+		assertEquals(2, folder.getProjects().size());
+
+		folder.getProjects().remove(project);
+		user.getProjects().remove(project);
+		project.setOwner(null);
+		project.setFolder(null);
+		entityManager.remove(project);
+		entityManager.flush();
+
+		assertEquals(1, folder.getProjects().size());
 		assertEquals(1, user.getProjects().size());
 
 		entityManager.getTransaction().commit();
